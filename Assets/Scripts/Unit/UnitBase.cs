aa3503b02ae7e93dbc4e8b0b36da4564635e3e99
fanak1 <minhtuanhust101@gmail.com>
2024-12-08 12:21:38 +0700

    8/12/2024
    uninstall some package

--- Assets/Scripts/Unit/UnitBase.cs
+++ Assets/Scripts/Unit/UnitBase.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-using MoreMountains.Tools;
 
 public abstract class UnitBase : MonoBehaviour {
 
